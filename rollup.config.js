5c531e842b49163a1f4d49452e2ae23a316c4863
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2019-07-01 00:31:47 +0800

    build(bundle): consider a UMD build

--- rollup.config.js
+++ rollup.config.js
@@ -1,59 +1,78 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import nodeResolve from "rollup-plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+import sourcemaps from "rollup-plugin-sourcemaps";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
 
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
+const input = "src/index.ts";
+const external = Object.keys(pkg.peerDependencies || {});
+const output = {
+  format: "umd",
+  sourcemap: true,
+  name: "ReactAutoControlled",
+  globals: {
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
+};
+const plugins = [
+  peerDepsExternal(),
+  nodeResolve(),
+  typescript(),
+  babel({
+    exclude: "node_modules/**",
+    plugins: ["external-helpers"]
+  }),
+  commonjs({
+    include: ["node_modules/**"],
+    namedExports: {
+      "node_modules/react/react.js": [
+        "Children",
+        "Component",
+        "PropTypes",
+        "createElement"
+      ],
+      "node_modules/react-dom/index.js": ["render"]
+    }
+  }),
+  sourcemaps(),
+  sizeSnapshot()
+];
+
+export default [
+  {
+    input: input,
+    output: {
+      ...output,
+      file: "dist/react-auto-controlled.js"
     },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true
+    external: external,
+    plugins: plugins
+  },
+  {
+    input: input,
+    output: {
+      ...output,
+      file: "dist/react-auto-controlled.min.js"
     },
-    {
+    external: external,
+    plugins: [...plugins, terser()]
+  },
+  {
+    input: input,
+    output: {
       file: "example/src/react-auto-controlled/index.js",
       format: "es",
+      sourcemap: true,
       banner: "/* eslint-disable */"
-    }
-  ],
-  external: Object.keys(pkg.peerDependencies || {}),
-  plugins: [
-    peerDepsExternal(),
-    nodeResolve({
-      browser: true
-    }),
-    babel({
-      exclude: "node_modules/**",
-      plugins: ["external-helpers"]
-    }),
-    typescript({
-      typescript: require("typescript")
-    }),
-    commonjs({
-      include: ["node_modules/**"],
-      namedExports: {
-        "node_modules/react/react.js": [
-          "Children",
-          "Component",
-          "PropTypes",
-          "createElement"
-        ],
-        "node_modules/react-dom/index.js": ["render"]
-      }
-    }),
-    sizeSnapshot()
-  ]
-};
+    },
+    external: external,
+    plugins: plugins,
+  }
+];
